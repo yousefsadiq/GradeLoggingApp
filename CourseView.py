9e8fe122ddbd7e1d6dc553f31dc5ee281b571a76
sobantahir <sobantahir13@gmail.com>
2025-11-21 09:55:16 -0500

    Added the add_course_new method to insert new rows with in progress formatting

--- CourseView.py
+++ CourseView.py
@@ -21,7 +21,7 @@ class CourseView:
         headers = ["Course Name", "Current Grade", "Desired Grade", "Required Grade", "Actions"]
 
         # Configure columns, first column gets more space, others get weight 1
-        self.grid_frame.grid_columnconfigure(0, weight=5)
+        self.grid_frame.grid_columnconfigure(0, weight=7)
         for i in range(1, len(headers)):
             self.grid_frame.grid_columnconfigure(i, weight=1)
 
@@ -31,11 +31,23 @@ class CourseView:
             tk.Label(self.grid_frame, text=text, font=("Arial", 12, "bold")).grid(
                 row=0, column=column_name, pady=(0,10), sticky="ew")
 
+        self.row_counter = 1 # first row after headers. used to track rows
+
         # To add a new course with the button to add on the bottom of the courses
         footer = tk.Frame(master)
         footer.pack(pady=20)
-        add_button = tk.Button(footer, text="➕", font=("Arial", 16))
+        add_button = tk.Button(footer, text="➕", font=("Arial", 16), command=self.add_course_new)
         add_button.pack()
+
+    def add_course_new(self):
+        # Widgets for each column header, including the button
+        tk.Entry(self.grid_frame, justify='center', font=('Arial', 10)).grid(row=self.row_counter, column=0, padx=5, pady=10, stick='ew')
+        tk.Label(self.grid_frame, justify='center', text='--%').grid(row=self.row_counter, column=1)
+        tk.Entry(self.grid_frame, justify='center').grid(row=self.row_counter, column=2)
+        tk.Label(self.grid_frame, justify='center', text='--%').grid(row=self.row_counter, column=3)
+        tk.Button(self.grid_frame, text='📝', font=('Arial', 11)).grid(row=self.row_counter, column=4, padx=5)
+
+        self.row_counter += 1
 
 if __name__ == "__main__":
     root = tk.Tk()
